efb050ab5357ea0d2663a64dd207ead2cb9efceb
Evan Grau <89275830+whatissatire@users.noreply.github.com>
2022-10-24 19:14:01 -0500

    response works

--- app/src/main/java/com/example/freetoplaygamesekg/App.java
+++ app/src/main/java/com/example/freetoplaygamesekg/App.java
@@ -1,38 +1,18 @@
 package com.example.freetoplaygamesekg;
 
 import android.app.Application;
-
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.toolbox.Volley;
+import android.content.Context;
 
 public class App extends Application {
+    private static Context mContext;
 
-    private RequestQueue requestQueue;
-    private static App mInstance;
-
+    @Override
     public void onCreate() {
         super.onCreate();
-        mInstance = this;
+        mContext = this;
     }
 
-    public static synchronized App getInstance() {
-        return mInstance;
-    }
-
-    public RequestQueue getRequestQueue() {
-        if (requestQueue == null) {
-            requestQueue = Volley.newRequestQueue(getApplicationContext());
-        }
-        return requestQueue;
-    }
-
-    public void addToRequestQueue(Request request, String tag) {
-        request.setTag(tag);
-        getRequestQueue().add(request);
-    }
-
-    public void cancelAllRequests(String tag) {
-        getRequestQueue().cancelAll(tag);
+    public static Context getContext() {
+        return mContext;
     }
 }
--- app/src/main/java/com/example/freetoplaygamesekg/MainActivity.java
+++ app/src/main/java/com/example/freetoplaygamesekg/MainActivity.java
@@ -9,10 +9,18 @@ import android.widget.Button;
 import android.widget.TextView;
 
 import com.android.volley.AuthFailureError;
+import com.android.volley.Cache;
+import com.android.volley.Network;
 import com.android.volley.Request;
+import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.BasicNetwork;
+import com.android.volley.toolbox.DiskBasedCache;
+import com.android.volley.toolbox.HurlStack;
 import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.google.gson.Gson;
 
 import org.json.JSONObject;
@@ -20,8 +28,6 @@ import org.json.JSONObject;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
-
-import okhttp3.OkHttpClient;
 
  public class MainActivity extends AppCompatActivity {
 
@@ -52,18 +58,31 @@ import okhttp3.OkHttpClient;
 
     private void getJson() {
         String url = "https://free-to-play-games-database.p.rapidapi.com/api/game?id=" + id;
-        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, url, null,
-                new Response.Listener<JSONObject>() {
+
+        RequestQueue requestQueue;
+
+        // Instantiate the cache
+        Cache cache = new DiskBasedCache(getCacheDir(), 1024 * 1024); // 1MB cap
+
+        // Set up the network to use HttpURLConnection as the HTTP client.
+        Network network = new BasicNetwork(new HurlStack());
+
+        // Instantiate the RequestQueue with the cache and network.
+        requestQueue = new RequestQueue(cache, network);
+
+        // Start the queue
+        requestQueue.start();
+
+        StringRequest request = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
                     @Override
-                    public void onResponse(JSONObject response) {
-//                        Gson gson = new Gson();
-//                        String json = String.valueOf(response);
-//                        model = gson.fromJson(json, GameModel.class);
+                    public void onResponse(String response) {
                         textView.setText("Response is: " + response);
                     }
                 }, new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError error) {
+                textView.setText("That didn't work!");
                 error.printStackTrace();
             }
         })
@@ -74,14 +93,13 @@ import okhttp3.OkHttpClient;
             @Override
             public Map<String, String> getHeaders() throws AuthFailureError {
                 Map<String, String> headers = new HashMap<>();
-                headers.put("X-RapidAPI-Host", "free-to-play-games-database.p.rapidapi.com");
                 headers.put("X-RapidAPI-Key", "3dd8cbd195msh5a03844d8648d59p144f23jsna6f80a74792b");
+                headers.put("X-RapidAPI-Host", "free-to-play-games-database.p.rapidapi.com");
                 return headers;
             }
         };
 
-        // Adding the request to the queue along with a unique string tag
-        App.getInstance().addToRequestQueue(jsonObjectRequest,"headerRequest");
+        requestQueue.add(request);
     }
 
     private void startNewActivity() {
